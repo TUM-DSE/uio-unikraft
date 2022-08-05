0c5dd3a184e42db35f42c16d2f260f02e62a2d1d
Masanori Misono <m.misono760@gmail.com>
2022-08-05 14:40:40 +0000

    lib/ukconsoledev: add template

---
+++ lib/ukconsoledev/console.c
@@ -0,0 +1,9 @@
+#include <uk/console.h>
+
+void uk_console_putc(char ch) {}
+
+char uk_console_getc()
+{
+	char a = 'a';
+	return a;
+}
---
+++ lib/ukconsoledev/include/uk/console.h
@@ -0,0 +1,15 @@
+#ifndef __UK_CONSOLE__
+#define __UK_CONSOLE__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void uk_console_putc(char);
+char uk_console_getc();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __UK_CONSOLE__ */
