3afdfd550dfba00e48c37011cef8d99c4896f04b
Masanori Misono <m.misono760@gmail.com>
2022-08-12 10:04:06 +0000

    lib/ushell: fix build scripts

---
+++ lib/ushell/include/ushell/ushell.h
@@ -0,0 +1,14 @@
+#ifndef __USHELL__
+#define __USHELL__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ushell_spawn_shell();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __USHELL__ */
---
+++ lib/ushell/ushell.c
@@ -0,0 +1,11 @@
+#include <uk/assert.h>
+#include <uk/console.h>
+#include <uk/essentials.h>
+#include <uk/print.h>
+
+#include <ushell/ushell.h>
+
+void ushell_spawn_shell()
+{
+	uk_pr_info("ushell spawn\n");
+}
