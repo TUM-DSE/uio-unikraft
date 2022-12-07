aee27ded0b22d1532ce8ce177826d77e65dea994
Masanori Misono <m.misono760@gmail.com>
2022-12-07 12:52:54 +0000

    lib/ushell: refactor

--- lib/ushell/loader.c
+++ lib/ushell/loader.c
@@ -1,6 +1,6 @@
 #include "elf.h"
 
-#include <string.h> // strncpy
+#include <string.h> // strncpy, memcpy, memset
 
 #ifdef USHELL_LOADER_TEST
 
@@ -12,8 +12,15 @@
 #include <sys/stat.h>
 #define UK_ASSERT assert
 #define USHELL_LOADER_PRRINT printf
+#if 1
 #define USHELL_LOADER_PR_DEBUG printf
+#else
+#define USHELL_LOADER_PR_DEBUG(...)                                            \
+	do {                                                                   \
+	} while (0)
+#endif
 #define USHELL_LOADER_PR_ERR printf
+#define USHELL_LOADER_PR_WARN printf
 
 void uk_console_puts(char *buf, int n);
 
@@ -40,6 +47,7 @@ void ushell_free_memory(void *addr, unsigned long size __attribute__((unused)))
 #define USHELL_LOADER_PRRINT uk_printk
 #define USHELL_LOADER_PR_DEBUG uk_pr_debug
 #define USHELL_LOADER_PR_ERR uk_pr_err
+#define USHELL_LOADER_PR_WARN uk_pr_warn
 
 #endif
 
@@ -58,6 +66,21 @@ struct ushell_program {
 	uint64_t entry_off;
 };
 
+struct elf_sections {
+	Elf64_Shdr *shstr;
+	Elf64_Shdr *str;
+	Elf64_Shdr *text;
+	Elf64_Shdr *sym;
+};
+
+struct ushell_loader_ctx {
+	void *elf_img;
+	size_t elf_size;
+	Elf64_Ehdr *ehdr;
+	struct elf_sections sections;
+	struct ushell_program *prog;
+};
+
 int ushell_program_current_idx;
 struct ushell_program ushell_programs[USHELL_PROG_MAX_NUM];
 
@@ -73,18 +96,21 @@ static struct ushell_program *ushell_program_find(char *name)
 	return NULL;
 }
 
-static void ushell_program_init(char *name, struct ushell_program *prog)
+static void ushell_program_init(struct ushell_loader_ctx *ctx, char *name)
 {
-	memcpy(prog->name, name, USHELL_PROG_NAME_MAX);
-	prog->text = NULL;
-	prog->data = NULL;
-	prog->bss = NULL;
-	prog->rodata = NULL;
-	prog->text_size = 0;
-	prog->data_size = 0;
-	prog->bss_size = 0;
-	prog->rodata_size = 0;
-	prog->entry_off = 0;
+	if (ushell_program_find(name) != NULL) {
+		USHELL_LOADER_PR_WARN("program %s is already loaded\n", name);
+	}
+	memcpy(ctx->prog->name, name, USHELL_PROG_NAME_MAX);
+	ctx->prog->text = NULL;
+	ctx->prog->data = NULL;
+	ctx->prog->bss = NULL;
+	ctx->prog->rodata = NULL;
+	ctx->prog->text_size = 0;
+	ctx->prog->data_size = 0;
+	ctx->prog->bss_size = 0;
+	ctx->prog->rodata_size = 0;
+	ctx->prog->entry_off = 0;
 }
 
 void *ushell_symbol_get(const char *symbol)
@@ -112,7 +138,40 @@ void uk_console_puts(char *buf, int n)
 	}
 }
 
-static void reloc_elf()
+static void ushell_program_free(int idx)
+{
+	assert(idx < USHELL_PROG_MAX_NUM);
+	struct ushell_program *prog = &ushell_programs[idx];
+	munmap(prog->text, prog->text_size);
+	munmap(prog->data, prog->data_size);
+	munmap(prog->bss, prog->bss_size);
+	munmap(prog->rodata, prog->rodata_size);
+	memset(prog, 0, sizeof(struct ushell_program));
+}
+
+static void ushell_program_free_all()
+{
+	int i;
+	for (i = 0; i < ushell_program_current_idx; i++) {
+		ushell_program_free(i);
+	}
+}
+
+static void dump_text(struct ushell_program *prog)
+{
+	size_t i;
+	char *p = prog->text;
+	printf("text size: %ld\n", prog->text_size);
+	for (i = 0; i < prog->text_size; i++) {
+		printf("%02X ", *p++ & 0xFF);
+		if ((i + 1) % 16 == 0) {
+			printf("\n");
+		}
+	}
+	printf("\n");
+}
+
+static int ushell_loader_elf_relocate_symbol(struct ushell_loader_ctx *ctx)
 {
 	/* NOTE: recent compiler uses R_X86_64_PLT32 instead of R_X86_64_PC32 to
 	 * mark 32-bit PC-relative branches.
@@ -129,79 +188,110 @@ static void reloc_elf()
 	 *    G: offset to the GOT relative to the address of the symbol
 	 *    GOT: address of the GOT
 	 */
+	return 0;
 }
 
-static void ushell_program_load_elf(char *path)
+static void ushell_loader_map_elf_image(struct ushell_loader_ctx *ctx,
+					char *path)
 {
-	assert(ushell_program_current_idx < USHELL_PROG_MAX_NUM);
-
-	struct ushell_program *prog =
-	    &ushell_programs[ushell_program_current_idx];
-	ushell_program_init(path, prog);
-
 	int fd = open(path, O_RDONLY);
 	assert(fd > 0);
 	struct stat sb;
 	fstat(fd, &sb);
 
 	printf("file: %s, size: %ld\n", path, sb.st_size);
-	void *elf_img = NULL;
-	size_t elf_size = sb.st_size;
-	elf_img = mmap(NULL, elf_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	assert(elf_img != MAP_FAILED);
+	ctx->elf_size = sb.st_size;
+	ctx->elf_img = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	assert(ctx->elf_img != MAP_FAILED);
+	ctx->ehdr = ctx->elf_img;
+}
 
-	Elf64_Ehdr *ehdr = elf_img;
-	assert(ehdr->e_ident[0] == 0x7f && ehdr->e_ident[1] == 'E'
-	       && ehdr->e_ident[2] == 'L' && ehdr->e_ident[3] == 'F');
-	assert(ehdr->e_machine == EM_AMD64);
-	assert(ehdr->e_type == ET_REL);
+static int ushell_loader_check_elf_header(struct ushell_loader_ctx *ctx)
+{
+	Elf64_Ehdr *ehdr = ctx->ehdr;
+	if (ehdr->e_ident[0] != 0x7f || ehdr->e_ident[1] != 'E'
+	    || ehdr->e_ident[2] != 'L' && ehdr->e_ident[3] != 'F') {
+		printf("Invalid elf magic number\n");
+		return -1;
+	}
+	if (ehdr->e_machine != EM_AMD64) {
+		printf("Unsupported elf machine: %d\n", ehdr->e_machine);
+		return -1;
+	}
+	if (ehdr->e_type != ET_REL) {
+		printf("Unsupported elf type: %d\n", ehdr->e_type);
+		return -1;
+	}
+	return 0;
+}
 
-	// scan sections
-	int i, txt_idx = -1, sym_idx = -1, str_idx = -1;
-	Elf64_Shdr *shdr, *shstr_shdr, *str_shdr, *txt_shdr, *sym_shdr;
-	printf("section num: %d\n", ehdr->e_shnum);
-	printf("string table idx: %d\n", ehdr->e_shstrndx);
-	shdr = elf_img + ehdr->e_shoff;
-	shstr_shdr = shdr + ehdr->e_shstrndx;
-	char *shstrtab = elf_img + shstr_shdr->sh_offset;
-	for (i = 0; i < ehdr->e_shnum; i++) {
+static int ushell_loader_elf_scan_section(struct ushell_loader_ctx *ctx)
+{
+	printf("section num: %d\n", ctx->ehdr->e_shnum);
+	printf("string table idx: %d\n", ctx->ehdr->e_shstrndx);
+	Elf64_Shdr *shdr = ctx->elf_img + ctx->ehdr->e_shoff;
+	ctx->sections.shstr = shdr + ctx->ehdr->e_shstrndx;
+	char *shstrtab = ctx->elf_img + ctx->sections.shstr->sh_offset;
+	int i;
+	for (i = 0; i < ctx->ehdr->e_shnum; i++) {
 		char *shname = shstrtab + shdr->sh_name;
 		printf("seciton %d: %s\n", i, shname);
 		if (!strcmp(shname, ".text")) {
-			txt_shdr = shdr;
-			txt_idx = i;
+			ctx->sections.text = shdr;
 		} else if (!strcmp(shname, ".symtab")) {
-			sym_shdr = shdr;
-			sym_idx = i;
+			ctx->sections.sym = shdr;
 		} else if (!strcmp(shname, ".strtab")) {
-			str_shdr = shdr;
-			str_idx = i;
+			ctx->sections.str = shdr;
 		}
 		shdr++;
 	}
-	assert(txt_idx != -1 && sym_idx != -1);
 
-	printf("found text section: id=%d, offset=%ld, size=%ld\n", txt_idx,
-	       txt_shdr->sh_offset, txt_shdr->sh_size);
+	if (ctx->sections.text == NULL) {
+		printf("could not find .text section\n");
+		return -1;
+	}
+	if (ctx->sections.sym == NULL) {
+		printf("could not find .symtab section\n");
+		return -1;
+	}
+	if (ctx->sections.str == NULL) {
+		printf("could not find .strtab section\n");
+		return -1;
+	}
+	return 0;
+}
 
-	// load each sections
-	prog->text = mmap(NULL, elf_size, PROT_WRITE | PROT_EXEC,
-			  MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	assert(prog->text != MAP_FAILED);
-	memcpy(prog->text, elf_img + txt_shdr->sh_offset, txt_shdr->sh_size);
+static int ushell_loader_elf_load_section(struct ushell_loader_ctx *ctx)
+{
+	ctx->prog->text_size = ctx->sections.text->sh_size;
+	ctx->prog->text =
+	    mmap(NULL, ctx->prog->text_size, PROT_WRITE | PROT_EXEC,
+		 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (ctx->prog->text == MAP_FAILED) {
+		printf("failed to mmap text section\n");
+		ctx->prog->text = NULL;
+		ctx->prog->text_size = 0;
+		return -1;
+	}
+	memcpy(ctx->prog->text, ctx->elf_img + ctx->sections.text->sh_offset,
+	       ctx->sections.text->sh_size);
+	return 0;
+}
 
-	// scan symbol section (search entry point)
-	int sym_entries = sym_shdr->sh_size / sym_shdr->sh_entsize;
-	printf(
-	    "found symbol table section: id=%d, offset=%ld, size=%ld, num=%d\n",
-	    sym_idx, sym_shdr->sh_offset, sym_shdr->sh_size, sym_entries);
-	Elf64_Sym *sym = elf_img + sym_shdr->sh_offset;
+static int ushell_loader_elf_scan_symbol(struct ushell_loader_ctx *ctx)
+{
+	int i;
+	int sym_entries =
+	    ctx->sections.sym->sh_size / ctx->sections.sym->sh_entsize;
+	Elf64_Sym *sym = ctx->elf_img + ctx->sections.sym->sh_offset;
 	Elf64_Sym *main_sym = NULL;
+
 	for (i = 0; i < sym_entries; i++, sym++) {
-		char *name = sym->st_name == 0
-				 ? "<noname>"
-				 : (char *)(elf_img + str_shdr->sh_offset
-					    + sym->st_name);
+		char *name =
+		    sym->st_name == 0
+			? "<noname>"
+			: (char *)(ctx->elf_img + ctx->sections.str->sh_offset
+				   + sym->st_name);
 		printf(
 		    "symbol %d: %s, size=%ld, value=%ld, type=%d, shndx=%x\n",
 		    i, name, sym->st_size, sym->st_value, sym->st_info & 0xf,
@@ -211,45 +301,98 @@ static void ushell_program_load_elf(char *path)
 		}
 	}
 
-	assert(main_sym);
-	prog->entry_off = main_sym->st_value;
+	if (main_sym == NULL) {
+		printf("no main symbol\n");
+		return -1;
+	}
 
+	ctx->prog->entry_off = main_sym->st_value;
+
+	return 0;
+}
+
+static int ushell_loader_load_elf(char *path)
+{
+	int ret = 0, r;
+	if (ushell_program_current_idx >= USHELL_PROG_MAX_NUM) {
+		printf("reach program loading limit\n");
+		return -1;
+	}
+
+	struct ushell_loader_ctx ctx = {};
+
+	ctx.prog = &ushell_programs[ushell_program_current_idx];
 	ushell_program_current_idx += 1;
-	munmap(elf_img, elf_size);
+	ushell_program_init(&ctx, path);
+
+	ushell_loader_map_elf_image(&ctx, path);
+
+	r = ushell_loader_check_elf_header(&ctx);
+	if (r != 0) {
+		printf("invalid program binary\n");
+		goto err;
+	}
+
+	r = ushell_loader_elf_scan_section(&ctx);
+	if (r != 0) {
+		printf("failed to scan sections\n");
+		goto err;
+	}
+
+	r = ushell_loader_elf_load_section(&ctx);
+	if (r != 0) {
+		printf("failed to load sections\n");
+		goto err;
+	}
+
+	r = ushell_loader_elf_scan_symbol(&ctx);
+	if (r != 0) {
+		printf("failed to scan symbols\n");
+		goto err;
+	}
+
+	r = ushell_loader_elf_relocate_symbol(&ctx);
+	if (r != 0) {
+		printf("failed to relocate symbols\n");
+		goto err;
+	}
+
+end:
+	munmap(ctx.elf_img, ctx.elf_size);
+	return ret;
+
+err:
+	ushell_program_current_idx -= 1;
+	ushell_program_free(ushell_program_current_idx);
+	ret = -1;
+	goto end;
 }
 
 static void ushell_program_run(char *prog_name, int argc, char *argv[])
 {
 	struct ushell_program *prog = ushell_program_find(prog_name);
-	assert(prog);
+	if (!prog) {
+		printf("program not found: %s\n", prog_name);
+		return;
+	}
+	dump_text(prog);
 	int (*func)(int, char *[]) = prog->text + prog->entry_off;
 	int r = func(argc, argv);
 	printf("return value: %d\n", r);
 }
 
-static void ushell_program_free()
-{
-	int i = 0;
-	for (i = 0; i < ushell_program_current_idx; i++) {
-		munmap(ushell_programs[i].text, ushell_programs[i].text_size);
-		munmap(ushell_programs[i].data, ushell_programs[i].data_size);
-		munmap(ushell_programs[i].bss, ushell_programs[i].bss_size);
-		munmap(ushell_programs[i].rodata,
-		       ushell_programs[i].rodata_size);
-	}
-}
-
 int main(int argc, char *argv[])
 {
 	if (argc == 1) {
-		printf("Usage: %s <prog> [option...]\n", argv[0]);
+		printf("Usage: %s <prog> [args...]\n", argv[0]);
 		return 0;
 	}
 	char *prog = argv[1];
 
-	ushell_program_load_elf(prog);
+	int r = ushell_loader_load_elf(prog);
+	assert(r == 0);
 	ushell_program_run(prog, argc - 1, argv + 1);
-	ushell_program_free();
+	ushell_program_free_all();
 
 	return 0;
 }
